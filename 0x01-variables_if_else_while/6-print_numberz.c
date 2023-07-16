53c387857201c448d2edab36e3dc40d1be1a926a
Bokang06 <bokangshogole45@gmail.com>
2023-07-15 17:45:51 -0700

    task06

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,4 @@
-#includ <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Print numbers from 0 to 9.
@@ -7,12 +7,14 @@
  */
 int main(void)
 {
-	int i;
+	int c;
 
-	for (i = 48; i < 58; i++)
-	{
-		putchar(i);
-	}
+	c =0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
 	putchar('\n');
 	return (0);
 }
