cfb92fef1ee56a6b2a6f44c3a8b8539ee2382862
Bokang06 <bokangshogole45@gmail.com>
2023-07-19 19:32:00 -0700

    102task

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints first 50 Fibonacci number, starting with 1 and 2,
- * separated by a comma followed by a space.
+ *        separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
@@ -26,6 +26,4 @@ int main(void)
 	}
 
 	return (0);
-
 }
-			
