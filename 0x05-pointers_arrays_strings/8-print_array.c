13161ee27a0350dea26b7cbbe6deea6629f9156e
Bokang06 <bokangshogole45@gmail.com>
2023-07-25 19:40:03 -0700

    printarr

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_array - a function that prints n elements of array
+ * print_array - a function that prints n elements of an array
  * @a: array name
  * @n: is the number of elemets OF the array to be printed
  * Return: a and n inputs
